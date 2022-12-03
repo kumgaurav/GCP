699e8665ea2b53ba55e9b132f2de30a47ec9e8ed
gaurav <gaurav@localhost>
2022-12-03 15:40:07 -0800

    updated the directory

---
+++ src/test/java/com/gkumargaur/oauth2/GcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkumargaur.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
